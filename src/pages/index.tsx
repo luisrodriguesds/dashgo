3ccce5cdc1aacbcccf148fe7a21c58dc098dda44
Luis <luisitaloar@gmail.com>
2021-03-31 20:52:59 -0300

    feat: tela login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,29 @@
+import { Flex, Button, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
 
 export default function Home() {
   return (
-    <div>
-      <h2>Dash</h2>
-    </div>
+    <Flex w="100%" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          
+          <Input name="email" label="E-mail" type="email" />
+
+          <Input name="password" label="Password" type="password" />
+
+          <Button type="button" size="lg" colorScheme="pink">
+            Entrar
+          </Button>
+        </Stack>
+      </Flex>
+    </Flex>
   )
 }
