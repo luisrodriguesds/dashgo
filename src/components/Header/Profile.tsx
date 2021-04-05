e4a9b22430b20be36fe5944b19474a227bfc22de
Luis <luisitaloar@gmail.com>
2021-04-05 20:07:53 -0300

    refactor: header mobile

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile(){
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps){
   return (
-    <Flex
-          align="center"
-        >
-      <Box textAlign="right" mr="4">
-        <Text>Luis Rodrigues</Text>
-        <Text color="gray.300" fontSize="small">
-        luis@luis.com
-        </Text>
-      </Box>
+    <Flex align="center">
+      {showProfileData && (
+        <Box textAlign="right" mr="4">
+          <Text>Luis Rodrigues</Text>
+          <Text color="gray.300" fontSize="small">
+          luis@luis.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Luis Rodrigues" src="https://github.com/luisrodriguesds.png" />
     </Flex>
