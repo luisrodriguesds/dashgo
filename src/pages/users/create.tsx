6b5542c4f03bf1a4dcef73cd421b990474fefe9e
Luis <luisitaloar@gmail.com>
2021-04-05 21:31:53 -0300

    feat: mobile version on the create user page

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -14,19 +14,19 @@ export default function UserCreate(){
           flex="1"
           borderRadius={8}
           bg="gray.800"
-          p="8"
+          p={["6", "8"]}
         >
             <Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
 
             <Divider my="6" borderColor="gray.700" />
 
             <VStack spacing="8">
-              <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
                 <Input name="name" label="Nome Completo" />
                 <Input name="email" type="email" label="E-mal" />
               </SimpleGrid>
 
-              <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
                 <Input name="password" type="password" label="Senha" />
                 <Input name="password_confirm" type="password" label="Confirmar Senha" />
               </SimpleGrid>
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -6,6 +6,11 @@ import { Sidebar } from "../../components/Sidebar";
 
 
 export default function UserList(){
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg : true
+  })
+
   return (
     <Box>
       <Header />
@@ -41,17 +46,17 @@ export default function UserList(){
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["4", "4", '6']} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
+                {isWideVersion && (<Th>Data de cadastro</Th>)}
                 <Th width="8"></Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", '6']}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -60,7 +65,7 @@ export default function UserList(){
                     <Text fontSize="sm" color="gray">Diego Fernandes</Text>
                   </Box>
                 </Td>
-                <Td>04 de Abril, 2021</Td>
+                {isWideVersion && (<Td>04 de Abril, 2021</Td>)}
                 <Td>
                   <Button
                     as="a"
@@ -68,9 +73,9 @@ export default function UserList(){
                     fontSize="sm"
                     colorScheme="purple"
                     cursor="pointer"
-                    leftIcon={<Icon as={RiPencilLine} />}
+                    leftIcon={<Icon as={RiPencilLine} marginInlineEnd={isWideVersion ? "0rem" : "-0.5rem"} />}
                   >
-                    Editar
+                    {isWideVersion && ('Editar')}
                   </Button>
                 </Td>
               </Tr>
