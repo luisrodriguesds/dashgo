2e51309a4cb89a37851f26f865e2852cb971904b
Luis <luisitaloar@gmail.com>
2021-04-05 21:43:28 -0300

    feat: activating links

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,16 +1,20 @@
-import { Icon, Link, Text, LinkProps as ChakaraLinkProps } from "@chakra-ui/react";
+import { Icon, Link as ChakaraLink, Text, LinkProps as ChakaraLinkProps } from "@chakra-ui/react";
 import { ElementType } from "react";
+import Link from "next/link";
 
 interface NavLinkProps extends ChakaraLinkProps {
   icon:ElementType;
   children: string;
+  href: string;
 }
 
-export function NavLink({ icon, children, ...rest }: NavLinkProps){
+export function NavLink({ icon, children, href, ...rest }: NavLinkProps){
   return (
-    <Link display="flex" alignItems="center" {...rest}>
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontWeight="medium">{children}</Text>
+    <Link href={href} passHref>    
+      <ChakaraLink display="flex" alignItems="center" {...rest}>
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontWeight="medium">{children}</Text>
+      </ChakaraLink>
     </Link>
   )
 }
