6e8739ff1a603604d899c5b7d1281444886ad67c
Luis <luisitaloar@gmail.com>
2021-04-06 00:20:06 -0300

    feat: react hook forms

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,5 @@
 import { Icon, Link as ChakaraLink, Text, LinkProps as ChakaraLinkProps } from "@chakra-ui/react";
 import { ElementType } from "react";
-import Link from "next/link";
 import { ActiveLink } from "./ActiveLink";
 
 interface NavLinkProps extends ChakaraLinkProps {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
 import { Flex, Button, Stack } from "@chakra-ui/react";
 import { Input } from "../components/Form/Input";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type SignInFormData = {
+  email: string;
+  password: string;
+}
 
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (data) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log(data);
+
+  }
+
   return (
     <Flex w="100%" h="100vh" align="center" justify="center">
       <Flex
@@ -12,14 +26,15 @@ export default function Home() {
         p="8"
         borderRadius={8}
         flexDirection="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
           
-          <Input name="email" label="E-mail" type="email" />
+          <Input name="email" label="E-mail" type="email" {...register('email')} />
 
-          <Input name="password" label="Password" type="password" />
+          <Input name="password" label="Password" type="password" {...register('password')} />
 
-          <Button type="button" size="lg" colorScheme="pink">
+          <Button type="submit" size="lg" colorScheme="pink" isLoading={formState.isSubmitting}>
             Entrar
           </Button>
         </Stack>
