c12154b03f8974ae76fbe96452cf67f130a7c611
Luis <luisitaloar@gmail.com>
2021-04-06 10:06:47 -0300

    feat: working with forms and validates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import { Flex, Button, Stack } from "@chakra-ui/react";
 import { Input } from "../components/Form/Input";
 import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 
 type SignInFormData = {
   email: string;
   password: string;
 }
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('Email obrigatório').email('Email inválido'),
+  password: yup.string().required('Senha obrigatória')
+})
+
 export default function Home() {
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  });
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (data) => {
     await new Promise(resolve => setTimeout(resolve, 2000));
@@ -18,6 +28,7 @@ export default function Home() {
 
   return (
     <Flex w="100%" h="100vh" align="center" justify="center">
+      {console.log(errors)}
       <Flex
         as="form"
         w="100%"
@@ -30,11 +41,23 @@ export default function Home() {
       >
         <Stack spacing="4">
           
-          <Input name="email" label="E-mail" type="email" {...register('email')} />
+          <Input 
+            name="email" 
+            label="E-mail" 
+            type="email" 
+            {...register('email')}
+            error={errors.email} 
+          />
 
-          <Input name="password" label="Password" type="password" {...register('password')} />
+          <Input 
+            name="password" 
+            label="Password" 
+            type="password" 
+            {...register('password')}
+            error={errors.password} 
+          />
 
-          <Button type="submit" size="lg" colorScheme="pink" isLoading={formState.isSubmitting}>
+          <Button type="submit" size="lg" colorScheme="pink" isLoading={isSubmitting}>
             Entrar
           </Button>
         </Stack>
